569118c3992c3ca740fd8f0da7db3282d55e1a68
tanisk jha <jhatanishqq@outlook.com>
2020-02-06 19:35:58 +0530

    last commit

---
+++ app.js
@@ -0,0 +1,32 @@
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+
+function getJokes(e) {
+  // Grabs just the number input type
+  const number = document.querySelector("input[type=\"number\"]").value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+            
+      let output = "";
+
+      if(response.type === "success") {
+        response.value.forEach(function(joke) {
+          output += `<p>${joke.joke}</p>`;
+        });
+      } else {
+        output += "<li>Oops! Something went wrong.</li>";
+      }
+
+      document.querySelector(".jokes").innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
